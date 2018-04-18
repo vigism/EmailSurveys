846499530ac7278292a0c5ac808adf2fd3c7b2de
V <r1vig@ucsd.edu>
2018-04-18 14:53:10 -0700

    first changes

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app=express();
 
 app.get('/',(req,res)=>{
-  res.send({hi:'there'});
+  res.send({
+      hi:'there',
+      new:'tester'});
 });
 
 const PORT = process.env.PORT||5000;
